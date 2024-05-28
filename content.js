2cfa6b1c682c1922dabb3f80299ece9ba3b4bf02
Nathan Donato <njdonato30@gmail.com>
2024-05-28 13:43:56 -0700

    first commit for prod

--- content.js
+++ content.js
@@ -1,16 +1,9 @@
-// working code but I take off some of the extra code that we don't really need in production 5/25/24
-// 5/27 noticed some inconsistencies with it detecting an addedNodes in the mutation.addedNodes when the observer is started.
-// document.querySelector(".ytp-skip-ad-button").click();
-// something else i could try using to observe "ytp-ad-persistent-progress-bar-container". I saw that when ad an plays the display proeprty changes from display none, to nothing
 // 5/28 working code now. Testing and then shipping to production and adding the monetization part using Mellowtel. Remove uncessary console logs.
-console.log("content script started.");
 
 {
   let intervalId;
-  // Function to click skip button
   function clickSkipButton() {
     intervalId = setInterval(function () {
-      console.log("Entered interval to click skip button.");
       const skip_button = document.querySelector(".ytp-skip-ad-button");
       if (skip_button) {
         console.log("Skip button found! Clicking it...");
@@ -19,29 +12,8 @@ console.log("content script started.");
     }, 1000);
   }
 
-  // check if ytp-ad-player-overlay-layout exists instead of checkiing for addedNodesLength and remvoedNodesLength instead of checking for mutations basically
-  // if ytp-ad-player-overlay-layout exists, then clickSkipButton() function else clearInterval()
-  // Function to handle mutations
-  function handleMutations(mutations) {
-    console.log("mutation detected. Mutations: ", mutations);
-
-    if (mutations[0].attributeName === "style") {
-      console.log("style attribute changed");
-      if (mutations[0].target.style.display === "") {
-        console.log("Ad started playing");
-        clickSkipButton();
-      } else {
-        console.log("Ad ended");
-        clearInterval(intervalId);
-      }
-    }
-  }
-
   // Function to handle changes in the style property of the target element
   function handleStyleChanges(mutationsList) {
-    console.log("handleStyleChanges detected.");
-    console.log("mutationsList: ", mutationsList);
-    console.log("mutationsList.style", mutationsList[0].target.style.display);
     if (mutationsList[0].target.style.display === "") {
       console.log("Ad started playing");
       clickSkipButton();
@@ -60,8 +32,6 @@ console.log("content script started.");
       ".ytp-ad-persistent-progress-bar-container"
     );
     if (adProgressBar) {
-      console.log("progress bar found");
-      console.log("adProgressBar display value: ", adProgressBar.style.display);
       if (adProgressBar.style.display === "") {
         clickSkipButton();
       }
@@ -72,8 +42,6 @@ console.log("content script started.");
       });
       console.log("observer started");
     } else {
-      console.log("progress bar not found");
-      console.log("Retrying...");
       setTimeout(startObserving, 500);
     }
   }
