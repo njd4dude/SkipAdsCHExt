9d13b84baae9e709d1fce596f52a407e2f7290e0
Nathan Donato <njdonato30@gmail.com>
2024-05-29 15:47:41 -0700

    removed console logs and remove bg script

--- content.js
+++ content.js
@@ -3,23 +3,18 @@
   let intervalId;
   function clickSkipButton() {
     intervalId = setInterval(function () {
-      console.log("skip interval");
       const skip_button = document.querySelector(".ytp-skip-ad-button");
       if (skip_button) {
         skip_button.click();
-        console.log("skip button clicked");
       }
     }, 1000);
   }
 
   // Function to handle changes in the style property of the target element
   function handleStyleChanges(mutationsList) {
-    console.log("style changes");
     if (mutationsList[0].target.style.display === "") {
-      console.log("ad found");
       clickSkipButton();
     } else {
-      console.log("ad ended");
       clearInterval(intervalId);
     }
   }
@@ -29,7 +24,6 @@
 
   // Function to start observing the target node
   function startObserving() {
-    console.log("start observing");
     const adProgressBar = document.querySelector(
       ".ytp-ad-persistent-progress-bar-container"
     );
@@ -42,23 +36,11 @@
         attributeOldValue: true, // Record old values
         attributeFilter: ["style"], // Only watch for changes in the 'style' attribute
       });
-      console.log("observing started");
     } else {
       setTimeout(startObserving, 500);
-      console.log("not found, retrying in 500ms");
     }
   }
 }
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("request: ", request);
-  console.log("sender: ", sender);
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  if (request.greeting === "hello") sendResponse({ farewell: "goodbye" });
-});
 
 // Start observing
 startObserving();
